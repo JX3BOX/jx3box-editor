c39212d13d2cbfaf8de9e667c66a96863a8b6962
iRuxu <rx6@qq.com>
2026-03-14 06:03:27 +0800

    fix:directory

--- src/pages/article.js
+++ src/pages/article.js
@@ -6,6 +6,7 @@ const app = createApp(ArticlePage);
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "@jx3box/jx3box-common/css/element-plus-theme.scss";
+import "@jx3box/jx3box-common/css/element-fonticon.css";
 app.use(ElementPlus);
 
 import '@jx3box/jx3box-common/css/font.css';
